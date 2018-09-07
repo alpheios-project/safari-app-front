29a0a1c8c9251c371fb28f84ffddbf7e470098d3
Irina Sklyarova <irina060981@yandex.ru>
2018-09-07 23:26:01 +1000

    apply all content process

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
-import ContentProcess from './content/content-process'
+import ContentProcess from '@/content/content-process'
 import ComponentStyles from '../node_modules/alpheios-components/dist/style/style.min.css' // eslint-disable-line
 
 document.addEventListener('DOMContentLoaded', (event) => {
   console.info('**********************DOMContentLoaded start', event.target)
   let contentProcess = new ContentProcess()
   contentProcess.initialize()
-  console.info('**********************DOMContentLoaded finish', contentProcess)
+  console.info('**********************DOMContentLoaded finish')
 })
---
+++ src/lib/messaging/message/state-message.js
@@ -0,0 +1,9 @@
+import Message from '@/lib/messaging/message/message'
+import TabScript from '@/lib/content/tab-script'
+
+export default class StateMessage extends Message {
+  constructor (state) {
+    super(TabScript.serializable(state))
+    this.type = Symbol.keyFor(Message.types.STATE_MESSAGE)
+  }
+}
