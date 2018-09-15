d3299492c2c35fc847c4785ee556bfd74d5be301
Irina Sklyarova <irina060981@yandex.ru>
2018-09-15 16:18:42 +1000

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 /* eslint-disable no-unused-vars */
 import ContentProcess from '@/content/content-process'
 import ComponentStyles from '../node_modules/alpheios-components/dist/style/style.min.css' // eslint-disable-line
+import { Monitor as ExperienceMonitor } from 'alpheios-experience'
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  console.info('**********************DOMContentLoaded start', event.target)
-  let contentProcess = new ContentProcess()
+/*  let contentProcess = new ContentProcess()
+  contentProcess.initialize() */
+  let contentProcess = ExperienceMonitor.track(
+    new ContentProcess(),
+    [
+      {
+        monitoredFunction: 'getWordDataStatefully',
+        experience: 'Get word data',
+        asyncWrapper: ExperienceMonitor.recordExperience
+      }
+    ]
+  )
+
   contentProcess.initialize()
-  console.info('**********************DOMContentLoaded finish')
 })
