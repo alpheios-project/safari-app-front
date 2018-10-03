8264dd9cc2add4ff645f43bcc49f44c90016d655
Irina Sklyarova <irina060981@yandex.ru>
2018-10-03 19:08:36 +1000

    update all libs

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 /* eslint-disable no-unused-vars */
 import ContentProcess from '@/content/content-process'
 import ComponentStyles from '../node_modules/alpheios-components/dist/style/style.min.css' // eslint-disable-line
-import { Monitor as ExperienceMonitor } from 'alpheios-experience' // eslint-disable-line
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  let contentProcess = ExperienceMonitor.track(
-    new ContentProcess(),
-    [
-      {
-        monitoredFunction: 'getWordDataStatefully',
-        experience: 'Get word data',
-        asyncWrapper: ExperienceMonitor.recordExperience
-      }
-    ]
-  )
-
+  let contentProcess = new ContentProcess()
   contentProcess.initialize()
 })
